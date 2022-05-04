3bc389f3ab489242d05b0b253628165c12863cf3
hiteshjogiya <hiteshjogiya2017@gmail.com>
2022-05-04 17:57:45 +0530

    1 binaries uploaded

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -6,6 +6,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
+
   post:any[]=[{key:"1",PostName:"Animal",PostDescription:"The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera.",PostedDate:"2022-06-02",PostedImage:"../assets/animal.jpg"},{key:"2",PostName:"City",PostDescription:"paris is the capital and most populous city of France, with an estimated population of 2,165,423 residents in 2019",PostedDate:"2022-06-02",PostedImage:"../assets/paris.jpg"},{key:"3",PostName:"Forest",PostDescription:"forest is a complex ecological system in which trees are the dominant life-form.",PostedDate:"2022-06-02",PostedImage:"../assets/forest.jpg"}]
   constructor() { }
   @Output() notifyme = new EventEmitter<any>();
